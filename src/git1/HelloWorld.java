4f7be3b6190df91715a8bb09cae884e219ab4a4c
EmranSDET <emran98627@gmail.com>
2019-06-18 17:57:16 -0400

    whats up git!

--- src/git1/HelloWorld.java
+++ src/git1/HelloWorld.java
@@ -4,9 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
-		
-		System.out.println("seeing the changes on github");
+		System.out.println("Hello Github!");
 		
 
 	}
